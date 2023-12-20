386c535c55ba33f5e3d39a3442938da3aceadb96
Paulina Loor Tubay <108084639+LoorPaulina@users.noreply.github.com>
2023-12-20 10:57:52 -0500

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { BibliotecaComponent } from './pages/biblioteca/biblioteca.component';
 import { IndexComponent } from './pages/index/index.component';
 
 export const routes: Routes = [
-    { path: 'index', component: IndexComponent },
+    { path: '', component: IndexComponent },
     { path: 'charts', component: ChartsComponent },
     { path: 'biblioteca', component: BibliotecaComponent },
-    { path: "**", redirectTo: 'index' }
+    { path: "**", redirectTo: '' }
 ];
