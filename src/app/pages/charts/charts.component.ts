56e34e2ff0255c2985eb2fca6ac6e18ad01b22f2
andresACF <107152458+andresACF@users.noreply.github.com>
2023-12-23 08:56:33 -0500

    Graficos

--- src/app/pages/charts/charts.component.ts
+++ src/app/pages/charts/charts.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 
+import { BuscadorComponent } from '../../shared/buscador/buscador.component';
+import { Pokemon } from '../../interfaces/pokemon';
+import { HttpClientModule } from '@angular/common/http';
+import { BuscaPokemonService } from '../../providers/busca-pokemon.service';
+
+
+
+
 @Component({
   selector: 'app-charts',
   standalone: true,
-  imports: [],
+  imports: [BuscadorComponent,HttpClientModule],
   templateUrl: './charts.component.html',
-  styleUrl: './charts.component.css'
+  styleUrl: './charts.component.css',
+  providers: [BuscaPokemonService]
+
 })
 export class ChartsComponent {
+  public data : Pokemon[] = [];
+  public url: string="";
+  public listUrls: string[]=[];
+
+  constructor(private dataProvider: BuscaPokemonService) { }
+  ngOnInit() {
+    this.dataProvider.getResponse().subscribe((response) => { 
+      let dataArray = (response as Pokemon[]); 
+      this.data = dataArray.slice(0,10);
+      console.log(dataArray.slice(0,10));
+  
+    })
+    
+  }
 
 }
